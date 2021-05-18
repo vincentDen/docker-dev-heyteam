6e6bd190e1da33ff040935650c638bf3d9d6fcb6
Vincent Denjoy <vincent.denjoy@heyteam.com>
2021-05-18 17:16:02 +0200

    .

---
+++ build/mysql_testing/init.sql
@@ -0,0 +1,5 @@
+GRANT ALL on dev3_company_api.* to master@'%';
+GRANT ALL on dev3_master_api.* to master@'%';
+
+CREATE DATABASE IF NOT EXISTS `dev3_master_api`;
+CREATE DATABASE IF NOT EXISTS `dev3_company_api`;
